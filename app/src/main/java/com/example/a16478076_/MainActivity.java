142892a922dfe5bd9c640f7c3dca55caa6da5f76
Flamse <1048605630@qq.com>
2019-09-21 12:56:45 +0800

    页面搭建
    还不能运行

---
+++ app/src/main/java/com/example/a16478076_/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.a16478076_;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_login);
+//        setContentView(R.layout.activity_register);
+//        setContentView(R.layout.activity_find);
+    }
+}
